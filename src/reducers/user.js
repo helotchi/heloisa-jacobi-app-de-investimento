c689a412006aaffae19a58a6d4e6bff2591dcb63
Heloisa <heloisajacobi@gmail.com>
2022-07-23 19:36:07 -0300

    Tela de Login - Definido state usuário

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,11 @@
 import { USER_SESSION } from '../actions';
 
 const INITIAL_STATE = {
+  codCliente: 0,
   email: '',
+  name: '',
+  balance: 0,
+  investedStocks: 0,
 };
 
 function userReducer(state = INITIAL_STATE, action) {
